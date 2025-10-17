20a736c15d889267292379cfebcf8cfb551d150e
hquach102 <hquach102@student.hbuhsd.edu>
2025-10-17 21:46:20 +0000

    sdssfdfd

--- Main.java
+++ Main.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class Main
 {
   public static void main(String[] args)
-  {
-    // write solutions to problems here
+    {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Give positive integers");
+
+    int sum = 0;
+    int scan = 0;
+
+    while(scan != -1)
+    {
+      scan = scan.nextInt();
+      sum += scan;
+    }
+    sum++;
+    System.out.println("The sum is: " + sum);
+
+    
+      }
   }
-}
+  
+
